9c79662631c935f5fbdae01d7e0fb7d4102cc33c
flabby <abhaybhat0312@gmail.com>
2021-11-23 15:36:19 +0530

    added API feature

--- backend/index.js
+++ backend/index.js
@@ -44,6 +44,126 @@ app.get("/courses", async (req, res) => {
     }
 })
 
+app.get("/certificate", async (req, res) => {
+    try {
+        //await
+        const allCourses = await pool.query("SELECT * FROM certificate;");
+        res.json(allCourses.rows);
+    } catch (err) {
+        console.error(err.message);
+    }
+})
+
+app.get("/degree_degree", async (req, res) => {
+    try {
+        //await
+        const allCourses = await pool.query("SELECT * FROM degree_degree;");
+        res.json(allCourses.rows);
+    } catch (err) {
+        console.error(err.message);
+    }
+})
+
+app.get("/enrolled_in", async (req, res) => {
+    try {
+        //await
+        const allCourses = await pool.query("SELECT * FROM enrolled_in;");
+        res.json(allCourses.rows);
+    } catch (err) {
+        console.error(err.message);
+    }
+})
+
+app.get("/faculty", async (req, res) => {
+    try {
+        //await
+        const allCourses = await pool.query("SELECT * FROM faculty;");
+        res.json(allCourses.rows);
+    } catch (err) {
+        console.error(err.message);
+    }
+})
+
+app.get("/faculty_contact", async (req, res) => {
+    try {
+        //await
+        const allCourses = await pool.query("SELECT * FROM faculty_contact;");
+        res.json(allCourses.rows);
+    } catch (err) {
+        console.error(err.message);
+    }
+})
+
+app.get("/library", async (req, res) => {
+    try {
+        //await
+        const allCourses = await pool.query("SELECT * FROM library;");
+        res.json(allCourses.rows);
+    } catch (err) {
+        console.error(err.message);
+    }
+})
+
+app.get("/mentor_system", async (req, res) => {
+    try {
+        //await
+        const allCourses = await pool.query("SELECT * FROM mentor_system;");
+        res.json(allCourses.rows);
+    } catch (err) {
+        console.error(err.message);
+    }
+})
+
+app.get("/stream", async (req, res) => {
+    try {
+        //await
+        const allCourses = await pool.query("SELECT * FROM stream;");
+        res.json(allCourses.rows);
+    } catch (err) {
+        console.error(err.message);
+    }
+})
+
+app.get("/student_contact", async (req, res) => {
+    try {
+        //await
+        const allCourses = await pool.query("SELECT * FROM student_contact;");
+        res.json(allCourses.rows);
+    } catch (err) {
+        console.error(err.message);
+    }
+})
+
+app.get("/student_course", async (req, res) => {
+    try {
+        //await
+        const allCourses = await pool.query("SELECT * FROM student_course;");
+        res.json(allCourses.rows);
+    } catch (err) {
+        console.error(err.message);
+    }
+})
+
+app.get("/studies", async (req, res) => {
+    try {
+        //await
+        const allCourses = await pool.query("SELECT * FROM studies;");
+        res.json(allCourses.rows);
+    } catch (err) {
+        console.error(err.message);
+    }
+})
+
+app.get("/teaches", async (req, res) => {
+    try {
+        //await
+        const allCourses = await pool.query("SELECT * FROM teaches;");
+        res.json(allCourses.rows);
+    } catch (err) {
+        console.error(err.message);
+    }
+})
+
 app.listen(3000, () => {
     console.log("server is listening on port 3000");
 })
